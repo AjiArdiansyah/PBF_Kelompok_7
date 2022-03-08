cfff8e3e40ca955157feb17259e958c531f86a30
bagusjackal <faridbagus14@gmail.com>
2022-03-08 22:39:44 +0700

    update

--- src/App.js
+++ src/App.js
@@ -13,4 +13,6 @@ class App extends Component {
       </div>
     )
   }
-}export default App;
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-// import App from './App';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
-// import Test from './Test';
-// import HelloComponent from './component/HelloComponent';
+import Test from './Test';
+import HelloComponent from './component/HelloComponent';
 import Login from './Login';
 import Register from './Register';
 
