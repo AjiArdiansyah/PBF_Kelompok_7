d240492c749786cd78c5c9172a746ee16c0f8635
Zhabiyan Wahyu Setyaputra <31399581+zhabiyan@users.noreply.github.com>
2022-03-09 09:40:52 +0700

    update form login

--- src/component/FormLogin.js
+++ src/component/FormLogin.js
@@ -2,24 +2,28 @@ import React from 'react'
 
 const Formlogin = () => {
     return (
-        <div style={{ marginTop: "100px" }}>
+        <div style={{ marginTop: "150px" }}>
             <div className="container">
                 <div className="row justify-content-center">
                     <div className="col-md-6">
-                        <h3 class="text-center p-2">Form Login</h3>
-                        <div className="card p-4">
+                        <h3 class="text-center p-3">Form Login</h3>
+                        <div className="card p-5">
                             <div className="card-body">
                                 <h5 class="card-title text-center">Tugas Pertemuan Ketiga</h5>
-                                <div className='form-group p-2'>
+                                <div className='form-group p-3'>
+
+                                    {/* username */}
                                     <label>Username</label>
-                                    <input placeholder=" Masukkan username" className="form-control"></input>
+                                    <input placeholder=" Masukkan Username" className="form-control"></input>
                                 </div>
-                                <div className='form-group p-2'>
+
+                                <div className='form-group p-3'>
                                     <label>Password</label>
-                                    <input type="password" placeholder="Masukkan password" className="form-control"></input>
+                                    <input type="password" placeholder="Masukkan Password Anda" className="form-control"></input>
                                 </div>
-                                <div className='d-grid gap-2 p-2'>
-                                    <div class="form-check">
+
+                                <div className='d-grid gap-2 p-3'>
+                                    <div class="form-check-center text-center">
                                         <input class="form-check-input" type="checkbox" value="" id="invalidCheck" required></input>
                                         <label class="form-check-label" for="invalidCheck">
                                             Remember Me
@@ -29,15 +33,15 @@ const Formlogin = () => {
                                         </div>
                                     </div>
                                 </div>
-                                <div className='d-grid gap-2 p-2'>
+                                <div className='d-grid gap-2 p-3'>
                                     <a href="#" class="btn btn-success">Login</a>
                                 </div>
 
-                                <div className='d-grid gap-2 p-2'>
+                                <div className='d-grid gap-2 p-3'>
                                     <a href="#register" class="btn btn-success">Register</a>
                                 </div>
                             
-                                <div className='d-grid gap-2 p-2'>
+                                <div className='d-grid gap-2 p-3'>
                                     <a href="#r" class="btn btn-danger">Cancel</a>
                                 </div>
                             </div>
