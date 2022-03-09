6231c9258eca36d87c0f97655bfeb463dc00e154
Zhabiyan Wahyu Setyaputra <31399581+zhabiyan@users.noreply.github.com>
2022-03-09 10:00:53 +0700

    update form login

--- src/component/FormLogin.js
+++ src/component/FormLogin.js
@@ -10,19 +10,18 @@ const Formlogin = () => {
                         <div className="card p-5">
                             <div className="card-body">
                                 <h5 class="card-title text-center">Tugas Pertemuan Ketiga</h5>
-                                <div className='form-group p-3'>
-
-                                    {/* username */}
+                                {/* Kolom */}
+                                <div className='form-group p-2'>                                    
                                     <label>Username</label>
                                     <input placeholder=" Masukkan Username" className="form-control"></input>
                                 </div>
 
-                                <div className='form-group p-3'>
+                                <div className='form-group p-2'>
                                     <label>Password</label>
                                     <input type="password" placeholder="Masukkan Password Anda" className="form-control"></input>
                                 </div>
 
-                                <div className='d-grid gap-2 p-3'>
+                                <div className='d-grid gap-2 p-2'>
                                     <div class="form-check-center text-center">
                                         <input class="form-check-input" type="checkbox" value="" id="invalidCheck" required></input>
                                         <label class="form-check-label" for="invalidCheck">
@@ -33,16 +32,15 @@ const Formlogin = () => {
                                         </div>
                                     </div>
                                 </div>
-                                <div className='d-grid gap-2 p-3'>
+                                {/* button */}
+                                <div className='d-grid gap-2 p-2'>
                                     <a href="#" class="btn btn-success">Login</a>
                                 </div>
-
-                                <div className='d-grid gap-2 p-3'>
-                                    <a href="#register" class="btn btn-success">Register</a>
-                                </div>
-                            
-                                <div className='d-grid gap-2 p-3'>
+                                <div className='d-grid gap-2 p-2'>
                                     <a href="#r" class="btn btn-danger">Cancel</a>
+                                </div>
+                                <div className='d-grid gap-2 p-2'>
+                                    <a href="#r" class="row justify-content-center">Belum Punya Akun?</a>
                                 </div>
                             </div>
                         </div>
