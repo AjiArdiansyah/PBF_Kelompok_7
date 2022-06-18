a9c0a298dfcbf81c0ac166567e8d6285f6a4a8fc
bagusjackal <76152181+bagusjackal@users.noreply.github.com>
2022-06-19 02:57:13 +0700

    login

--- src/App.js
+++ src/App.js
@@ -5,8 +5,9 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 //import List from './List';
 import Front from './Front/Front';
 import Back from './Back/Back';
+import Login from './Back/Login';
 import './index.css';
-import Nav from './Front/Nav';
+//import Nav from './Front/Nav';
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
 // class App extends Component {
@@ -30,10 +31,7 @@ function App() {
                 <Route path="/" component={Front} exact/>
                 <Route path="/home" component={Front} />
                 <Route path="/admin" component={Back} />
-
-                
-
-                
+                <Route path="/login" component={Login} />    
             </Router>
 
         </div>
